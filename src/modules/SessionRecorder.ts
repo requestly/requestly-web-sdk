96d00ef2af42f630cded83d4cc714b312dff2a5b
Vaibhav Nigam <lazyvab@gmail.com>
2022-05-20 13:44:37 +0530

    Use rrweb for video recording

--- src/modules/SessionRecorder.ts
+++ src/modules/SessionRecorder.ts
@@ -1,42 +1,52 @@
+import { record } from 'rrweb';
+import { eventWithTime } from 'rrweb/typings/types';
+
 export interface SessionOptions {
   video?: boolean;
   networkRequests?: boolean;
 }
 
 export interface SessionData {
+  url: string;
   startTime: number;
   stopTime: number;
+  videoEvents?: eventWithTime[];
 }
 
 export class SessionRecorder {
   #options: SessionOptions;
-  #startTime: number;
-  #stopTime: number;
+  #session: SessionData = {
+    url: '',
+    startTime: null,
+    stopTime: null,
+    videoEvents: [],
+  };
 
   constructor(options: SessionOptions) {
     this.#options = options || {
       video: true,
       networkRequests: true,
     };
+    this.#session.url = window.location.href;
   }
 
   start(): void {
-    this.#startTime = Date.now();
-    console.log('Starting session recording...');
+    this.#session.startTime = Date.now();
+
     if (this.#options.video) {
-      console.log('Recording video in session...');
+      record({
+        emit(event) {
+          this.#session.videoEvents.push(event);
+        },
+      });
     }
   }
 
   stop(): void {
-    this.#stopTime = Date.now();
-    console.log('Stopped session recording');
+    this.#session.stopTime = Date.now();
   }
 
   getData(): SessionData {
-    return {
-      startTime: this.#startTime,
-      stopTime: this.#stopTime,
-    };
+    return this.#session;
   }
 }
